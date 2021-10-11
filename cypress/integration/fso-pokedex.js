3d5a3303c3484057f2748ec13d5eff67a293a0ba
Azryel <AzryelShroud@gmail.com>
2021-10-11 21:41:46 +0200

    11.9 Simple end to end -tests

---
+++ cypress/integration/fso-pokedex.js
@@ -0,0 +1,34 @@
+describe('Pokedex', function() {
+    it('front page can be opened', function() {
+      cy.visit('http://localhost:5000')
+      cy.contains('ivysaur')
+      cy.contains('Pokémon and Pokémon character names are trademarks of Nintendo.')
+    })
+
+    beforeEach(function() {    
+        cy.visit('http://localhost:5000')  
+    })
+
+    it('specific pokemon page can be opened', function() {
+        cy.contains('charizard').click()
+        cy.contains('solar power')
+        cy.contains('blaze')
+    })
+
+    it('pokemon navigation works', function() {
+        cy.contains('charmeleon').click()
+        cy.contains('solar power')
+        cy.contains('blaze')
+
+        cy.contains('Previous').click()
+        cy.contains('charmander')
+
+        cy.contains('Next').click()
+        cy.contains('Next').click()
+        cy.contains('charizard')
+
+        cy.contains('Home').click()
+        cy.contains('squirtle')
+    })
+
+  })
